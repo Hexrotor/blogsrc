ee5040156b2294679348ba2be2d65fc70b7ce71e
Hexrotor <tszbbb@qq.com>
2022-12-16 21:47:02 +0800

    build

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Use for local test
+cp -r post_images/* node_modules/hexo-theme-redefine/source/images/post_imgs
+hexo clean
+git add -A
+git commit -m "build"
+git push
+rm -r node_modules/hexo-theme-redefine/source/images/post_imgs/*
