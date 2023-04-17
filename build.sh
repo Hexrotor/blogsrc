b6acdf19368fe213844422137b4e265f0db25bd1
Hexrotor <tszbbb@qq.com>
2023-04-17 20:55:55 +0800

    build

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 # Use for local test
 cp -ruf post_imgs/* node_modules/hexo-theme-redefine/source/images/post_imgs
-echo "Replacing imgs/pic.jpg to jsdelivr link"
-new="https://gcore.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
-old="imgs/"
-ls -d source/_posts/* | xargs -i sed -i "s@\(\!\[.*\](\)$old@\1$new@g" {}
-echo "Done"
+
+cat replace.sh|bash
+
 hexo clean
+
 git add -A
 git commit -m "build"
 git push
--- replace.sh
+++ replace.sh
@@ -1,3 +1,5 @@
-new="https://gcore.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
-old="https://test1.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
+echo "Replacing imgs/pic.jpg to jsdelivr link"
+new="https://testingcf.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
+old="imgs/"
 ls -d source/_posts/* | xargs -i sed -i "s@\(\!\[.*\](\)$old@\1$new@g" {}
+echo "Done"
