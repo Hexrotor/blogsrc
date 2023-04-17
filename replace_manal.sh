e4ffc55b5bac0a6f3228ce7003828d16cdc37696
Hexrotor <tszbbb@qq.com>
2023-04-17 21:22:23 +0800

    build

--- replace_manal.sh
+++ replace_manal.sh
@@ -1,5 +1,6 @@
 echo "Replacing imgs/pic.jpg to jsdelivr link"
-new="https://testingcf.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
-old="https://gcore.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/post_imgs/"
+new="https://testingcf.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/"
+old="https://gcore.jsdelivr.net/gh/hexrotor/hexrotor.github.io/images/"
 ls -d source/_posts/* | xargs -i sed -i "s@\(\!\[.*\](\)$old@\1$new@g" {}
+sed -i "s@$old@$new@g" _config.redefine.yml
 echo "Done"
